518c5223c59fd6d0f66b3c320fdb8233c938ae1e
Dexter <tomasribeiro2001@gmail.com>
2024-06-01 01:40:29 +0100

    Further work on player controller
    Did some more stuff on the player controller. More to come.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,65 +1,379 @@
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public PlayerInformation Information;
+	public PlayerInformation Information;
 
-    #region COMPONENTS
-    public Rigidbody2D rb {  get; private set; }
-    #endregion
+	#region COMPONENTS
+	public Rigidbody2D RB { get; private set; }
+	#endregion
 
-    #region STATE PARAMETERS
-    public bool IsFacingRight { get; private set; }
-    public bool IsJumping { get; private set; }
-    //timers
-    public float LastOnGroundTime { get; private set; }
-    //jump
-    //Jump
-    private bool _isJumpCut;
-    private bool _isJumpFalling;
-    #endregion
+	#region STATE PARAMETERS
+	public bool IsFacingRight { get; private set; }
+	public bool IsJumping { get; private set; }
+	public bool IsDashing { get; private set; }
 
-    #region INPUT
-    private Vector2 _moveInput;
-    public float LastPressedJumpTime { get; private set; }
-    #endregion
+	public float LastOnGroundTime { get; private set; }
 
-    #region CHECK PARAMETERS
-    //Set all of these up in the inspector
-    [Header("Checks")]
-    [SerializeField] private Transform _groundCheckPoint;
-    //Size of groundCheck depends on the size of your character generally you want them slightly small than width (for ground) and height (for the wall check)
-    [SerializeField] private Vector2 _groundCheckSize = new Vector2(0.49f, 0.03f);
-    #endregion
+	//Jump
+	private bool _isJumpCut;
+	private bool _isJumpFalling;
 
-    #region LAYERS & TAGS
-    [Header("Layers & Tags")]
-    [SerializeField] private LayerMask _groundLayer;
-    #endregion
+	//Dash
+	private int _dashesLeft;
+	private bool _dashRefilling;
+	private Vector2 _lastDashDir;
+	private bool _isDashAttacking;
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-    void Start()
-    {
-        SetGravityScale(Information.gravityScale);
-        IsFacingRight = true;
-    }
+	#endregion
 
-    void Update()
-    {
-        #region TIMERS
+	#region INPUT PARAMETERS
+	private Vector2 _moveInput;
 
-        #endregion
-    }
+	public float LastPressedJumpTime { get; private set; }
+	public float LastPressedDashTime { get; private set; }
+	#endregion
 
-    #region GENERAL METHODS
-    public void SetGravityScale(float scale)
-    {
-        rb.gravityScale = scale;
-    }
-    #endregion
+	#region CHECK PARAMETERS
+	[Header("Checks")]
+	[SerializeField] private Transform _groundCheckPoint;
+	[SerializeField] private Vector2 _groundCheckSize = new Vector2(0.49f, 0.03f);
+	#endregion
+
+	#region LAYERS & TAGS
+	[Header("Layers & Tags")]
+	[SerializeField] private LayerMask _groundLayer;
+	#endregion
+
+	private void Awake()
+	{
+		RB = GetComponent<Rigidbody2D>();
+	}
+
+	private void Start()
+	{
+		SetGravityScale(Information.gravityScale);
+		IsFacingRight = true;
+	}
+
+	private void Update()
+	{
+		#region TIMERS
+		LastOnGroundTime -= Time.deltaTime;
+
+		LastPressedJumpTime -= Time.deltaTime;
+		LastPressedDashTime -= Time.deltaTime;
+		#endregion
+
+		#region INPUT HANDLER
+		_moveInput.x = Input.GetAxisRaw("Horizontal");
+		_moveInput.y = Input.GetAxisRaw("Vertical");
+
+		if (_moveInput.x != 0)
+			CheckDirectionToFace(_moveInput.x > 0);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			OnJumpInput();
+		}
+
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			OnJumpUpInput();
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			OnDashInput();
+		}
+		#endregion
+
+		#region COLLISION CHECKS
+		if (!IsDashing && !IsJumping)
+		{
+			//Ground Check
+			if (Physics2D.OverlapBox(_groundCheckPoint.position, _groundCheckSize, 0, _groundLayer))
+			{
+				LastOnGroundTime = Information.coyoteTime;
+			}
+		}
+		#endregion
+
+		#region JUMP CHECKS
+		if (IsJumping && RB.velocity.y < 0)
+		{
+			IsJumping = false;
+
+			_isJumpFalling = true;
+		}
+
+		if (LastOnGroundTime > 0 && !IsJumping)
+		{
+			_isJumpCut = false;
+
+			_isJumpFalling = false;
+		}
+
+		if (!IsDashing)
+		{
+			//Jump
+			if (CanJump() && LastPressedJumpTime > 0)
+			{
+				IsJumping = true;
+				_isJumpCut = false;
+				_isJumpFalling = false;
+				Jump();
+			}
+		}
+		#endregion
+
+		#region DASH CHECKS
+		if (CanDash() && LastPressedDashTime > 0)
+		{
+			Sleep(Information.dashSleepTime);
+
+			if (_moveInput != Vector2.zero)
+				_lastDashDir = _moveInput;
+			else
+				_lastDashDir = IsFacingRight ? Vector2.right : Vector2.left;
+
+
+
+			IsDashing = true;
+			IsJumping = false;
+			_isJumpCut = false;
+
+			StartCoroutine(nameof(StartDash), _lastDashDir);
+		}
+		#endregion
+
+		#region GRAVITY
+		if (!_isDashAttacking)
+		{
+			if (RB.velocity.y < 0 && _moveInput.y < 0)
+			{
+				RB.velocity = new Vector2(RB.velocity.x, Mathf.Max(RB.velocity.y, -Information.maxFallSpeed));
+			}
+			else if (_isJumpCut)
+			{
+				SetGravityScale(Information.gravityScale * Information.jumpCutGravityMult);
+				RB.velocity = new Vector2(RB.velocity.x, Mathf.Max(RB.velocity.y, -Information.maxFallSpeed));
+			}
+			else if ((IsJumping || _isJumpFalling) && Mathf.Abs(RB.velocity.y) < Information.jumpHangTimeThreshold)
+			{
+				SetGravityScale(Information.gravityScale * Information.jumpHangGravityMult);
+			}
+			else if (RB.velocity.y < 0)
+			{
+				SetGravityScale(Information.gravityScale * Information.fallGravityMult);
+				RB.velocity = new Vector2(RB.velocity.x, Mathf.Max(RB.velocity.y, -Information.maxFallSpeed));
+			}
+			else
+			{
+				SetGravityScale(Information.gravityScale);
+			}
+		}
+		else
+		{
+			SetGravityScale(0);
+		}
+		#endregion
+	}
+
+	private void FixedUpdate()
+	{
+		if (!IsDashing)
+		{
+			Run(1);
+		}
+		else if (_isDashAttacking)
+		{
+			Run(Information.dashEndRunLerp);
+		}
+	}
+
+	#region INPUT CALLBACKS
+	public void OnJumpInput()
+	{
+		LastPressedJumpTime = Information.jumpInputBufferTime;
+	}
+
+	public void OnJumpUpInput()
+	{
+		if (CanJumpCut())
+			_isJumpCut = true;
+	}
+
+	public void OnDashInput()
+	{
+		LastPressedDashTime = Information.dashInputBufferTime;
+	}
+	#endregion
+
+	#region GENERAL METHODS
+	public void SetGravityScale(float scale)
+	{
+		RB.gravityScale = scale;
+	}
+
+	private void Sleep(float duration)
+	{
+		StartCoroutine(nameof(PerformSleep), duration);
+	}
+
+	private IEnumerator PerformSleep(float duration)
+	{
+		Time.timeScale = 0;
+		yield return new WaitForSecondsRealtime(duration);
+		Time.timeScale = 1;
+	}
+	#endregion
+
+	//MOVEMENT METHODS
+	#region RUN METHODS
+	private void Run(float lerpAmount)
+	{
+		float targetSpeed = _moveInput.x * Information.runMaxSpeed;
+		targetSpeed = Mathf.Lerp(RB.velocity.x, targetSpeed, lerpAmount);
+
+		#region Calculate AccelRate
+		float accelRate;
+
+		if (LastOnGroundTime > 0)
+			accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? Information.runAccelAmount : Information.runDeccelAmount;
+		else
+			accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? Information.runAccelAmount * Information.accelInAir : Information.runDeccelAmount * Information.deccelInAir;
+		#endregion
+
+		#region Add Bonus Jump Apex Acceleration
+		if ((IsJumping || _isJumpFalling) && Mathf.Abs(RB.velocity.y) < Information.jumpHangTimeThreshold)
+		{
+			accelRate *= Information.jumpHangAccelerationMult;
+			targetSpeed *= Information.jumpHangMaxSpeedMult;
+		}
+		#endregion
+
+		#region Conserve Momentum
+		if (Information.doConserveMomentum && Mathf.Abs(RB.velocity.x) > Mathf.Abs(targetSpeed) && Mathf.Sign(RB.velocity.x) == Mathf.Sign(targetSpeed) && Mathf.Abs(targetSpeed) > 0.01f && LastOnGroundTime < 0)
+		{
+			accelRate = 0;
+		}
+		#endregion
+
+		float speedDif = targetSpeed - RB.velocity.x;
+
+		float movement = speedDif * accelRate;
+
+		RB.AddForce(movement * Vector2.right, ForceMode2D.Force);
+	}
+
+	private void Turn()
+	{
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+
+		IsFacingRight = !IsFacingRight;
+	}
+	#endregion
+
+	#region JUMP METHODS
+	private void Jump()
+	{
+		LastPressedJumpTime = 0;
+		LastOnGroundTime = 0;
+
+		#region Perform Jump
+		float force = Information.jumpForce;
+		if (RB.velocity.y < 0)
+			force -= RB.velocity.y;
+
+		RB.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+		#endregion
+	}
+	#endregion
+
+	#region DASH METHODS
+	//Dash Coroutine
+	private IEnumerator StartDash(Vector2 dir)
+	{
+
+		LastOnGroundTime = 0;
+		LastPressedDashTime = 0;
+
+		float startTime = Time.time;
+
+		_dashesLeft--;
+		_isDashAttacking = true;
+
+		SetGravityScale(0);
+
+		while (Time.time - startTime <= Information.dashAttackTime)
+		{
+			RB.velocity = dir.normalized * Information.dashSpeed;
+			yield return null;
+		}
+
+		startTime = Time.time;
+
+		_isDashAttacking = false;
+
+		SetGravityScale(Information.gravityScale);
+		RB.velocity = Information.dashEndSpeed * dir.normalized;
+
+		while (Time.time - startTime <= Information.dashEndTime)
+		{
+			yield return null;
+		}
+
+		//Dash over
+		IsDashing = false;
+	}
+
+	private IEnumerator RefillDash(int amount)
+	{
+		_dashRefilling = true;
+		yield return new WaitForSeconds(Information.dashRefillTime);
+		_dashRefilling = false;
+		_dashesLeft = Mathf.Min(Information.dashAmount, _dashesLeft + 1);
+	}
+	#endregion
+
+	#region CHECK METHODS
+	public void CheckDirectionToFace(bool isMovingRight)
+	{
+		if (isMovingRight != IsFacingRight)
+			Turn();
+	}
+
+	private bool CanJump()
+	{
+		return LastOnGroundTime > 0 && !IsJumping;
+	}
+
+	private bool CanJumpCut()
+	{
+		return IsJumping && RB.velocity.y > 0;
+	}
+
+	private bool CanDash()
+	{
+		if (!IsDashing && _dashesLeft < Information.dashAmount && LastOnGroundTime > 0 && !_dashRefilling)
+		{
+			StartCoroutine(nameof(RefillDash), 1);
+		}
+
+		return _dashesLeft > 0;
+	}
+	#endregion
+
+
+	#region EDITOR METHODS
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(_groundCheckPoint.position, _groundCheckSize);
+	}
+	#endregion
 }
--- Assets/Scripts/PlayerInformation.cs
+++ Assets/Scripts/PlayerInformation.cs
@@ -3,70 +3,80 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Player Information")]
 public class PlayerInformation : ScriptableObject
 {
-    [Header("Gravity")]
-    [HideInInspector] public float gravityStrength;
-    [HideInInspector] public float gravityScale;
+	[Header("Gravity")]
+	[HideInInspector] public float gravityStrength;
+	[HideInInspector] public float gravityScale;
 
-    [Space(5)]
-    public float fallGravityMult;
-    public float maxFallSpeed;
+	[Space(5)]
+	public float fallGravityMult;
+	public float maxFallSpeed;
 
-    [Space(20)]
-    [Header("Run")]
-    public float runMaxSpeed;
-    public float runAcceleration;
-    [HideInInspector] public float runAccelAmount;
-    public float runDecceleration;
-    [HideInInspector] public float runDeccelAmount;
-    [Space(5)]
-    [Range(0f, 1)] public float accelInAir;
-    [Range(0f, 1)] public float deccelInAir;
-    [Space(5)]
-    public bool doConserveMomentum = true;
+	[Space(20)]
 
-    [Space(20)]
+	[Header("Run")]
+	public float runMaxSpeed;
+	public float runAcceleration;
+	[HideInInspector] public float runAccelAmount;
+	public float runDecceleration;
+	[HideInInspector] public float runDeccelAmount;
+	[Space(5)]
+	[Range(0f, 1)] public float accelInAir;
+	[Range(0f, 1)] public float deccelInAir;
+	[Space(5)]
+	public bool doConserveMomentum = true;
 
-    [Header("Jump")]
-    public float jumpHeight;
-    public float jumpTimeToApex;
-    [HideInInspector] public float jumpForce;
+	[Space(20)]
 
-    [Header("Early Jump Stops")]
-    public float jumpCutGravityMult;
-    [Range(0f, 1)] public float jumpHangGravityMult;
-    public float jumpHangTimeThreshold;
-    [Space(0.5f)]
-    public float jumpHangAccelerationMult;
-    public float jumpHangMaxSpeedMult;
+	[Header("Jump")]
+	public float jumpHeight;
+	public float jumpTimeToApex;
+	[HideInInspector] public float jumpForce;
 
-    [Space(20)]
+	[Header("Both Jumps")]
+	public float jumpCutGravityMult;
+	[Range(0f, 1)] public float jumpHangGravityMult;
+	public float jumpHangTimeThreshold;
+	[Space(0.5f)]
+	public float jumpHangAccelerationMult;
+	public float jumpHangMaxSpeedMult;
 
-    [Header("Slide")]
-    public float slideSpeed;
-    public float slideAccel;
+	[Space(20)]
 
-    [Header("Assists")]
-    [Range(0.01f, 0.5f)] public float coyoteTime;
-    [Range(0.01f, 0.5f)] public float jumpInputBufferTime;
+	[Header("Assists")]
+	[Range(0.01f, 0.5f)] public float coyoteTime;
+	[Range(0.01f, 0.5f)] public float jumpInputBufferTime;
 
-    private void OnValidate()
-    {
-        //Calculate gravity strength using the formula (gravity = 2 * jumpHeight / timeToJumpApex^2) 
-        gravityStrength = -(2 * jumpHeight) / (jumpTimeToApex * jumpTimeToApex);
+	[Space(20)]
 
-        //Calculate the rigidbody's gravity scale (ie: gravity strength relative to unity's gravity value, see project settings/Physics2D)
-        gravityScale = gravityStrength / Physics2D.gravity.y;
+	[Header("Dash")]
+	public int dashAmount;
+	public float dashSpeed;
+	public float dashSleepTime;
+	[Space(5)]
+	public float dashAttackTime;
+	[Space(5)]
+	public float dashEndTime;
+	public Vector2 dashEndSpeed;
+	[Range(0f, 1f)] public float dashEndRunLerp;
+	[Space(5)]
+	public float dashRefillTime;
+	[Space(5)]
+	[Range(0.01f, 0.5f)] public float dashInputBufferTime;
 
-        //Calculate are run acceleration & deceleration forces using formula: amount = ((1 / Time.fixedDeltaTime) * acceleration) / runMaxSpeed
-        runAccelAmount = (50 * runAcceleration) / runMaxSpeed;
-        runDeccelAmount = (50 * runDecceleration) / runMaxSpeed;
+	private void OnValidate()
+	{
+		gravityStrength = -(2 * jumpHeight) / (jumpTimeToApex * jumpTimeToApex);
 
-        //Calculate jumpForce using the formula (initialJumpVelocity = gravity * timeToJumpApex)
-        jumpForce = Mathf.Abs(gravityStrength) * jumpTimeToApex;
+		gravityScale = gravityStrength / Physics2D.gravity.y;
 
-        #region Variable Ranges
-        runAcceleration = Mathf.Clamp(runAcceleration, 0.01f, runMaxSpeed);
-        runDecceleration = Mathf.Clamp(runDecceleration, 0.01f, runMaxSpeed);
-        #endregion
-    }
+		runAccelAmount = (50 * runAcceleration) / runMaxSpeed;
+		runDeccelAmount = (50 * runDecceleration) / runMaxSpeed;
+
+		jumpForce = Mathf.Abs(gravityStrength) * jumpTimeToApex;
+
+		#region Variable Ranges
+		runAcceleration = Mathf.Clamp(runAcceleration, 0.01f, runMaxSpeed);
+		runDecceleration = Mathf.Clamp(runDecceleration, 0.01f, runMaxSpeed);
+		#endregion
+	}
 }

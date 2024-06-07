325e7de885832a7a3b9a1f8e715381cd7035445e
Fernando Cunha de Sousa <A042789@ipmaia.pt>
2024-06-07 17:24:13 +0100

    completed pause menu, help menu and level menu

---
+++ Assets/Scripts/LevelMenuController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenuController : MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Level2() 
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void Level3() 
+    {
+        SceneManager.LoadScene("Level3");
+    }
+}
--- Assets/Scripts/PauseMenuController.cs
+++ Assets/Scripts/PauseMenuController.cs
@@ -5,23 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenuController : MonoBehaviour
 {
-    public void ResumeGame()
+    public GameObject openMenu;
+    public GameObject pauseMenu;
+    public GameObject helpMenu;
+
+    public void OpenPauseMenu()
     {
-        
+        openMenu.SetActive(false);
+        pauseMenu.SetActive(true);
+        helpMenu.SetActive(false);
     }
 
-    public void HelpMenu()
+    public void ResumeGame()
     {
+        openMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+        helpMenu.SetActive(false);
+    }
 
+    public void OpenHelpMenu()
+    {
+        openMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+        helpMenu.SetActive(true);
     }
 
     public void RestartGame()
     {
-        SceneManager.LoadScene("Nando");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    public void QuitGame()
+    public void ExitGame()
     {
-        Application.Quit();
+        SceneManager.LoadScene("MenuStart");
     }
 }
